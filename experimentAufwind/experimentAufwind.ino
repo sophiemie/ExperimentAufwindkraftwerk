2956be19d972500476a21e26fde57c0106075c09
sophiemie <smiessner@stud.hs-bremen.de>
2024-03-14 00:23:55 +0100

    Projekt angelegt

---
+++ experimentAufwind/experimentAufwind.ino
@@ -0,0 +1,15 @@
+/*
+* meetMINT Space
+* Experiment 30. - 31.07.2024
+* 
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
